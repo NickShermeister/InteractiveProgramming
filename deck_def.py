4859627587483fdcb6eb965aa88d61a4326b23ae
Nate Sampo <nathan.sampo@students.olin.edu>
2017-02-27 15:56:12 -0500

    Added deck functionality

---
+++ deck_def.py
@@ -0,0 +1,21 @@
+import random
+
+class Deck(object):
+
+    def __init__(self, deck_size):
+        #deck_size is 36
+        self.deck_size = deck_size
+        card_attributes = {'suit':[0,1,2,3], 'number':[6,7,8,9,10,11,12,13,14]}
+        cards_in_deck = []
+        for s in card_attributes['suit']:
+            for n in card_attributes['number']:
+                #temp_card = Card(s, n)
+                #cards_in_deck.append(temp_card)
+                cards_in_deck.append([s, n])
+        print(cards_in_deck)
+
+    def draw(self):
+
+
+    def shuffle_deck(self, cards_in_deck):
+        random.shuffle(cards_in_deck)
