4ec78a3294f939d01546fd4ee92118747b7ce6be
Nick <ncsherman159@gmail.com>
2017-03-02 15:39:26 -0500

    I'm trying to get this text up...

--- field_def.py
+++ field_def.py
@@ -3,6 +3,8 @@ import card_game
 import deck_def
 import hand_def
 import pygame
+import time
+import game_constants
 
 import sys
 from pygame.locals import *
@@ -11,14 +13,22 @@ BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+pygame.init()
+FONT = pygame.font.SysFont(None, 25)
 
 class CardView(object):
     def __init__(self, model):
         self.model = model
+        self.font = pygame.font.SysFont('Arial', 25)
 
     def draw(self, surface):
         model = self.model
         pygame.draw.rect(surface, BLUE, (int(model.x), int(model.y), model.width, model.height))
+        #pygame.display.set_caption('Box Test')
+        screen_text = FONT.render('Test', True, GREEN)
+        checkr = pygame.display.set_mode((game_constants.window_height,game_constants.window_width))
+        pygame.display.blit(screen_text, [model.x, model.y])
 
 class DeckView(object):
     def __init__(self,model):
@@ -42,7 +52,7 @@ class MoveController(object):
             for model in self.models:
                 model.reset()
 
-class DrawController(object):
+class DeckController(object):
     def __init__(self, models):
         self.models = models
 
@@ -58,6 +68,7 @@ class DrawController(object):
 
 
 def main():
+    clock = pygame.time.Clock()
     pygame.init()
     screen = pygame.display.set_mode((640, 480))
 
@@ -90,6 +101,7 @@ def main():
             view.draw(screen)
 
         pygame.display.update()
+        clock.tick(80)
 
     pygame.quit()
 
