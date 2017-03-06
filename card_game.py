618416010f530ad364f07beb3e57f3ade7999d4a
Nick <ncsherman159@gmail.com>
2017-03-06 14:20:00 -0500

    I don't know what changes I made sorry?

--- card_game.py
+++ card_game.py
@@ -95,5 +95,7 @@ if __name__ == "__main__":
                 controllers = controllers[1:]
                 models = models[1:]
                 deckalive = False
+        if not deckalive:
+
 
     pygame.quit()
